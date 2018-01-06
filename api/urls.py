132f01b7ecbe343b8ce65f50941e7952a8b99df4
xiaoren <466565029@qq.com>
2018-01-06 18:12:46 +0800

    [swxs] 灵活添加url模块

--- api/urls.py
+++ api/urls.py
@@ -2,16 +2,11 @@
 
 from tornado.web import url
 import views as views
-from api.tag import views as tag_views
+from api.user import urls as user_urls
 
-url_mapping = [
-    url(r"/api/", views.IndexHandler, name='index'),
-    url(r"/api/tag/(\w+)/", tag_views.TagHandler, name='api_select_tag'),
-    url(r"/api/tag/", tag_views.TagHandler, name='api_select_tags'),
-    url(r"/api/tag/", tag_views.TagHandler, name='api_create_tag'),
-    url(r"/api/tag/(\w+)/", tag_views.TagHandler, name='api_update_tag'),
-    url(r"/api/tag/(\w+)/", tag_views.TagHandler, name='api_modify_tag'),
-    url(r"/api/tag/(\w+)/", tag_views.TagHandler, name='api_delete_tag'),
-]
-
-# url_mapping.append(tag_urls)
+def get_api_urls():
+    url_mapping = [
+        url(r"/api/", views.IndexHandler, name='index'),
+    ]
+    url_mapping.extend(user_urls.url_mapping)
+    return url_mapping
