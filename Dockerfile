f665e850eac0163c3510df7c84ebbcb448c20d37
swxs <466565029@qq.com>
2020-10-27 22:09:21 +0800

    [swxs] feature: 先简单打个包

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM swxs/home_base
 WORKDIR /home
-EXPOSE 8090:8090
 COPY . .
--- get_base.sh
+++ get_base.sh
@@ -1,5 +1,9 @@
 hash=`md5sum ./requirements.in | cut -c 1-16`
-docker build -f Dockerfile.dev -t swxs/home_base:$hash . 
+
+docker build -f Dockerfile.base -t swxs/home_base:$hash . 
 docker push swxs/home_base:$hash
 docker image tag swxs/home_base:$hash swxs/home_base:latest
 docker push swxs/home_base:latest
+
+docker build -t swxs/home .
+docker push swxs/home
