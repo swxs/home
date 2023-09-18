c9b09949322d643317600afcc2c30cb66b7a7c17
swxs <466565029@qq.com>
2023-09-18 15:14:51 +0800

    [swxs] feature: 不再使用model_consts, 目前使用consts

--- apps/password_lock/password_lock_utils.py
+++ apps/password_lock/password_lock_utils.py
@@ -6,16 +6,16 @@
 from commons.Helpers import encryption
 
 # 本模块方法
-from . import model_consts
+from . import consts
 
 
 async def get_password(password_lock):
-    if password_lock.ttype == model_consts.PASSWORD_LOCK_TTYPE_COMMON:
+    if password_lock.ttype == consts.PASSWORD_LOCK_TTYPE_COMMON:
         if password_lock.key:
             password = encryption.get_password(name=password_lock.key)
         else:
             password = None
-    elif password_lock.ttype == model_consts.PASSWORD_LOCK_TTYPE_CUSTOM:
+    elif password_lock.ttype == consts.PASSWORD_LOCK_TTYPE_CUSTOM:
         password = password_lock.custom.get('password', None)
     else:
         password = None
