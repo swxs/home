b65f08e03831f127e26181ba34a90b2b50f9da93
swxs <466565029@qq.com>
2026-08-06 11:01:50 +0800

    [vibe] feature: 时间类型返回格式优化

--- web/response.py
+++ web/response.py
@@ -22,10 +22,10 @@ def encoder(obj):
         return obj.model_dump()
     elif isinstance(obj, ObjectId):
         return str(obj)
-    elif isinstance(obj, datetime.date):
-        return obj.strftime("%Y/%m/%d")
     elif isinstance(obj, datetime.datetime):
         return obj.strftime("%Y/%m/%d %H:%M:%S")
+    elif isinstance(obj, datetime.date):
+        return obj.strftime("%Y/%m/%d")
     elif isinstance(obj, bytes):
         return obj.decode("utf8")
     else:
